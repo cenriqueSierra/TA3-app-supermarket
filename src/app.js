2fb7ebd93b10ef8b0583e74035fb8c48ac3f0599
Carlos Sierra <cesierra@espol.edu.ec>
2023-01-08 21:29:11 -0500

    Ajuste de puerto para el servidor

--- src/app.js
+++ src/app.js
@@ -5,13 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.set('port', process.env.PORT | 3000);
+
 app.get('/', (req,res) => {
     res.send("Servidor de supermarket")
 });
+app.use('/queries',require('./routes/queries'));
 
-const PORT = 3000;
-
-app.listen(PORT, () =>{
-
-});
-console.log(`Server on port ${PORT}`);
+app.listen(app.get('port'));
+console.log(`Server running on port ${PORT}`);
